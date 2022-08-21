a54a26176fef6f8448b7401b72290be84269e0af
simonll4 <sllamosas806@alumnos.iua.edu.ar>
2022-08-21 19:49:23 -0300

    secante

--- metodos_numericos/secante.cpp
+++ metodos_numericos/secante.cpp
@@ -5,43 +5,30 @@
 using namespace std;
 
 double funcion(double);
-double derivada(double);
 
 // funcion a estudiar
-
 double funcion(double x)
 {
-    return pow(x, 3) - x - 1;
-}
-
-double derivada(double x)
-{
-    return (3 * pow(x, 2)) - 1;
+    return pow(x, 2) - 3 * x - 4;
 }
 
 int main()
 {
-    double xViejo = 1;
-    double xNuevo, eAproximado, ePorcentual;
+    double xNuevo, eAproximado;
+    double xViejo = 5;
+    double xViejoViejo = 7;
     double tolerancia = pow(10, -5);
     int iteraciones = 0;
 
     do
     {
-        if (derivada(xViejo) < 0.0004)
-        {
-            cout << "Warning: la derivada de la funcion es demasiano cercana a cero, probar con otro metodo" << endl;
-            return 0;
-        }
-
-        xNuevo = xViejo - (funcion(xViejo) / derivada(xViejo));
+        xNuevo = xViejo - (funcion(xViejo) * (xViejoViejo - xViejo)) / (funcion(xViejoViejo) - funcion(xViejo));
         eAproximado = fabs(xNuevo - xViejo);
+        xViejoViejo = xViejo;
         xViejo = xNuevo;
         iteraciones++;
-
     } while (eAproximado > tolerancia || iteraciones > 10000);
 
     cout << "Raiz: " << xNuevo << "\nError: " << eAproximado << "\nIteraciones: " << iteraciones << endl;
-    cout << " f(xNuevo)=" << funcion(xNuevo) << endl;
     return 0;
 }
