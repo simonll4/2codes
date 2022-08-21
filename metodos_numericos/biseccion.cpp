89cd2bff8365a7c0d3e7052bc99ba405ea6279d2
simonll4 <sllamosas806@alumnos.iua.edu.ar>
2022-08-21 15:48:01 -0300

    biseccion

---
+++ metodos_numericos/biseccion.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <math.h>
+#include <stdlib.h>
+
+using namespace std;
+
+double funcion(double);
+
+// funcion a estudiar
+double funcion(double x)
+{
+    return 2 * x - 4;
+}
+
+int main()
+{
+
+    // intervalo que abarca la raiz
+    double a = 1;
+    double b = 4;
+
+    double cn, cv;
+    double eAproximado, ePorcentual = 0;
+    double tolerancia = pow(10, -5);
+    int iteraciones = 0;
+
+    if (funcion(a) * funcion(b) < 0)
+    {
+        do
+        {
+            if (iteraciones == 0)
+            {
+                cv = a;
+                cn = (a + b) / 2;
+                eAproximado = fabs(cn - cv);
+            }
+            else
+            {
+                if (funcion(a) * funcion(cn) < 0)
+                {
+                    b = cn;
+                    cv = cn;
+                    cn = (a + b) / 2;
+                    eAproximado = fabs(cn - cv);
+                }
+                else
+                {
+                    a = cn;
+                    cv = cn;
+                    cn = (a + b) / 2;
+                    eAproximado = fabs(cn - cv);
+                }
+            }
+            iteraciones++;
+        } while (eAproximado > tolerancia);
+    }
+    else
+    {
+        cout << "La funcion no tiene raices o  la misma no se encuentra en el intervalo elegido" << endl;
+    }
+
+    cout << "Raiz: " << cn << "\nError aproximado: " << eAproximado << "\nIteraciones: " << iteraciones << endl;
+
+    return 0;
+}
